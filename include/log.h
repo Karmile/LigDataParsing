c882856b02910db38ea44bcb37bc957d4bab1227
ChengPeng <2727675713@qq.com>
2024-04-02 14:50:46 +0800

    Change the log output method to daily separation

--- include/log.h
+++ include/log.h
@@ -5,11 +5,13 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <mutex>
 #include <string>
+
 class Logger {
  public:
   static Logger& GetInstance() {
@@ -19,6 +21,8 @@ class Logger {
   static bool Init(const std::string& file_name);
   template <typename T>
   static void TryLog(const T& value);
+  template <typename T>
+  static void TryLogDaily(const T& value);
 
   template <typename T>
   friend Logger& operator<<(Logger& logger, const T& value);
@@ -27,60 +31,41 @@ class Logger {
   static std::ofstream file_;
   static std::ostream console_;
   static std::mutex mutex_;
-  static std::string log_dir_;
   static std::string file_path_;
-  static int log_backup_index_;
-  static int file_counts_;
+
   Logger() {}
   Logger(const Logger&) = delete;
   Logger& operator=(const Logger&) = delete;
 };
 
-#define MAX_LOG_FILE_SIZE (20 * 1024 * 1024)
-// 设定日志文件的最大备份数量为 5
-#define MAX_LOG_BACKUPS (5)
-
 template <typename T>
-void Logger::TryLog(const T& value) {
+void Logger::TryLogDaily(const T& value) {
   std::unique_lock<std::mutex> lock(mutex_);
-  // 获取日志文件的当前大小
-  int64_t log_file_size = 0;
-  if (file_.is_open()) {
-    file_.seekp(0, std::ios::end);
-    log_file_size = file_.tellp();
-  } else {
-    log_file_size = 0;
+  auto current_time = std::time(nullptr);
+  // 格式化日期字符串
+  char buffer[80];
+  std::strftime(buffer, sizeof(buffer), "%Y%m%d", gmtime(&current_time));
+  // 将格式化后的字符串转换为 std::string 对象
+  std::string file_name(buffer);
+  auto File_name = file_path_ + "/" + file_name.substr(0, 6);
+  // 查找当前日期log文件是否存在
+  if (!filesystem::exists(File_name)) {
+    filesystem::create_directory(File_name);
   }
-  // 如果日志文件的大小超过了最大大小，则进行日志文件的滚动和备份
-  if (log_file_size >= MAX_LOG_FILE_SIZE) {
+  File_name += "/" + file_name + ".log";
+  if (!filesystem::exists(File_name)) {
     file_.close();
-    // 重命名当前的日志文件为备份文件
-    std::string backup_log_file_name = file_path_ + std::to_string(log_backup_index_) + ".log";
-    std::rename(file_path_.c_str(), backup_log_file_name.c_str());
-
-    file_.open(file_path_, std::ios::trunc);
-
-    // 删除最老的日志文件备份
-    if (file_counts_ >= MAX_LOG_BACKUPS) {
-      log_backup_index_ = (log_backup_index_) % MAX_LOG_BACKUPS + 1;
-      std::string oldest_log_file_name = file_path_ + std::to_string(log_backup_index_) + ".log";
-      remove(oldest_log_file_name.c_str());
-      file_counts_--;
-      log_backup_index_--;
-    }
-
-    // 更新日志文件备份索引
-    log_backup_index_++;
-    file_counts_++;
-  } else {
-    file_ << value;
   }
+  if (!file_.is_open()) {
+    file_.open(File_name, std::ios::app);
+  }
+  file_ << value;
 }
 
 template <typename T>
 Logger& operator<<(Logger& logger, const T& value) {
   logger.console_ << value;
-  logger.TryLog(value);
+  logger.TryLogDaily(value);
   return logger;
 }
 #define RAW_LOG(level, message)                                                              \
@@ -99,5 +84,42 @@ Logger& operator<<(Logger& logger, const T& value) {
 #define LOG_WARN(message) RAW_LOG("LOG_WARN", message)
 #define LOG_ERROR(message) RAW_LOG("LOG_ERROR", message)
 #define LOG_FATAL(message) RAW_LOG("LOG_FATAL", message)
+
+//template <typename T>
+//void Logger::TryLog(const T& value) {
+//  std::unique_lock<std::mutex> lock(mutex_);
+//  // 获取日志文件的当前大小
+//  int64_t log_file_size = 0;
+//  if (file_.is_open()) {
+//    file_.seekp(0, std::ios::end);
+//    log_file_size = file_.tellp();
+//  } else {
+//    log_file_size = 0;
+//  }
+//  // 如果日志文件的大小超过了最大大小，则进行日志文件的滚动和备份
+//  if (log_file_size >= MAX_LOG_FILE_SIZE) {
+//    file_.close();
+//    // 重命名当前的日志文件为备份文件
+//    std::string backup_log_file_name = file_path_ + std::to_string(log_backup_index_) + ".log";
+//    std::rename(file_path_.c_str(), backup_log_file_name.c_str());
+//
+//    file_.open(file_path_, std::ios::trunc);
+//
+//    // 删除最老的日志文件备份
+//    if (file_counts_ >= MAX_LOG_BACKUPS) {
+//      log_backup_index_ = (log_backup_index_) % MAX_LOG_BACKUPS + 1;
+//      std::string oldest_log_file_name = file_path_ + std::to_string(log_backup_index_) + ".log";
+//      remove(oldest_log_file_name.c_str());
+//      file_counts_--;
+//      log_backup_index_--;
+//    }
+//
+//    // 更新日志文件备份索引
+//    log_backup_index_++;
+//    file_counts_++;
+//  } else {
+//    file_ << value;
+//  }
+//}
 
 #endif  // LOG_H
--- log.cpp
+++ log.cpp
@@ -1,20 +1,16 @@
 #include "log.h"
 
-bool Logger::Init(const std::string& file_name) {
-  file_path_ = file_name;
-  size_t last_slash_pos = file_path_.find_last_of("/\\");
-  log_dir_ = file_path_.substr(0, last_slash_pos + 1);
-  std::unique_lock<std::mutex> lock(mutex_);
-  file_.open(file_path_, std::ios::app);
-  log_backup_index_ = 1;
-  file_counts_ = 1;
-  return file_.is_open();
+
+bool Logger::Init(const std::string& log_path) {
+  file_path_ = log_path;
+  auto su = true;
+  if (!std::filesystem::exists(file_path_)) {
+    su = std::filesystem::create_directory(file_path_);
+  }
+  return su;
 }
 
 std::ofstream Logger::file_;
 std::mutex Logger::mutex_;
-std::string Logger::log_dir_;
 std::string Logger::file_path_;
 std::ostream Logger::console_(std::cout.rdbuf());
-int Logger::log_backup_index_ = 1;
-int Logger::file_counts_ = 1;
