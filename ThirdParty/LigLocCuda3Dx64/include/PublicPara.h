37dc3d3a271f915f8e11c7c236f2da4498f23f86
Zilong QIN <zilong.qin@gmail.com>
2024-06-03 18:02:44 +0800

    更新LocSta参数，以及配套的Lib库文件。

--- ThirdParty/LigLocCuda3Dx64/include/PublicPara.h
+++ ThirdParty/LigLocCuda3Dx64/include/PublicPara.h
@@ -7,35 +7,33 @@
 #define BLOCK_X 32
 #define BLOCK_Y 32
 #define BLOCK_Z 1
-#define E_R 6374.004
+#define E_R 6371.004
 #define DATAPREC float
 #define SAVE_PATH "./Err_Result.txt"
 
-struct LocPara{
-	DATAPREC Lat;
-	DATAPREC boundW;
-	DATAPREC boundE;
-	DATAPREC boundS;
-	DATAPREC boundN;
-	DATAPREC boundhb;
-	DATAPREC boundht;
-	DATAPREC dh;
+struct LocPara {
+  DATAPREC Lat;
+  DATAPREC boundW;
+  DATAPREC boundE;
+  DATAPREC boundS;
+  DATAPREC boundN;
+  DATAPREC boundhb;
+  DATAPREC boundht;
+  DATAPREC dh;
 };
 
-struct LocSta{
-	LocSta(DATAPREC InLat, DATAPREC InLon, DATAPREC Inh, DATAPREC time = 0)
-	{
-		Lat = InLat;
-		Lon = InLon;
-		h = Inh;
-		occur_t = time;
-	}
-	LocSta()
-	{
-	}
-	DATAPREC Lat;
-	DATAPREC Lon;
-	DATAPREC h;
-	DATAPREC sq;
-	DATAPREC occur_t;
+struct LocSta {
+  LocSta(DATAPREC InLat, DATAPREC InLon, DATAPREC Inh, DATAPREC time = 0) {
+    Lat = InLat;
+    Lon = InLon;
+    h = Inh;
+    occur_t = time;
+  }
+  LocSta() {}
+  DATAPREC Lat;
+  DATAPREC Lon;
+  DATAPREC h;
+  DATAPREC sq;
+  DATAPREC occur_t;
+  DATAPREC confidence;
 };
